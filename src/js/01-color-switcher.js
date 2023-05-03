a227139d308fdcde8a6edc03f3ae2293ae60bfa5
Roman Shvagla <121064413+immortalua96@users.noreply.github.com>
2023-05-03 13:24:55 +0200

    Initial commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,20 @@
+const btnStart = document.querySelector('[data-start]')
+const btnStop = document.querySelector('[data-stop]')
+let timerId = null;
+btnStop.disable = true 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
